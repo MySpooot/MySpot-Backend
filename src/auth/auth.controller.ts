82f0e6bbec0ebc288bca019fc13fa488c22dc17b
InJaEE <45154110+InJaEE@users.noreply.github.com>
2022-01-02 21:43:17 +0900

    App Login API

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,34 +1,39 @@
-import { Body, Controller, Get, Param, Post, Put, UseGuards ,Headers} from '@nestjs/common';
+import {Body, Controller, Get, Param, Post, Put, UseGuards, Headers} from '@nestjs/common';
 
-import { JwtAuthGuard } from '../lib/jwt_guard';
-import { AuthUser, User_ } from '../lib/user_decorator'
-import { AuthService } from './auth.service';
-import { PostLoginBody, PostLoginHeaders } from './dto/post_login.dto';
-import { PutUserBody, PutUserParam } from './dto/put_user.dto';
+import {JwtAuthGuard} from '../lib/jwt_guard';
+import {AuthUser, User_} from '../lib/user_decorator';
+import {AuthService} from './auth.service';
+import {PostLoginBody, PostLoginHeaders} from './dto/post_login.dto';
+import {PutUserBody, PutUserParam} from './dto/put_user.dto';
 
 @Controller('/auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+    constructor(private readonly authService: AuthService) {}
 
-  @Post('/login')
-  login(@Headers() headers: PostLoginHeaders, @Body() body: PostLoginBody){
-    return this.authService.login(headers, body);
-  }
+    @Post('/login')
+    login(@Headers() headers: PostLoginHeaders, @Body() body: PostLoginBody) {
+        return this.authService.login(headers, body);
+    }
 
-  @Get('/me') 
-  @UseGuards(JwtAuthGuard)
-  me(@User_() user: AuthUser){
-    return this.authService.me(user);
-  }
+    @Get('/me')
+    @UseGuards(JwtAuthGuard)
+    me(@User_() user: AuthUser) {
+        return this.authService.me(user);
+    }
 
-  @Put('/user/:userId')
-  updateUser(@Param() param: PutUserParam, @Body() body: PutUserBody){
-    return this.authService.updateUser(param, body)
-  }
+    @Put('/user/:userId')
+    updateUser(@Param() param: PutUserParam, @Body() body: PutUserBody) {
+        return this.authService.updateUser(param, body);
+    }
 
-  @Post('/logout')
-  @UseGuards(JwtAuthGuard)
-  logout(@Body() body){
-    return this.authService.logout(body)
-  }
+    @Post('/logout')
+    @UseGuards(JwtAuthGuard)
+    logout(@Body() body) {
+        return this.authService.logout(body);
+    }
+
+    @Post('/appLogin')
+    appLogin(@Body() body) {
+        return this.authService.loginProcess(body);
+    }
 }
