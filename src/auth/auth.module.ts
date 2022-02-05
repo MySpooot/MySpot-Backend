f690f67ef06dcb642b4a67ef801f69793abb4f9e
2-chanhee <l01083582454@gmail.com>
2022-02-05 17:50:10 +0900

    jwt lib 수정

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,22 +1,14 @@
 import { Module } from '@nestjs/common';
+import { HttpModule } from '@nestjs/axios';
+import { ConfigModule } from '@nestjs/config';
 
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { HttpModule } from '@nestjs/axios';
-import { ConfigModule } from '@nestjs/config';
-import { JwtStrategy } from '../lib/jwt.strategy';
-import { PassportModule } from '@nestjs/passport';
-import { JwtModule } from '@nestjs/jwt';
-import configuration from '../configuration';
+import { JwtAuthModule } from '../lib/jwt';
 
 @Module({
-    imports: [
-        ConfigModule,
-        HttpModule,
-        PassportModule,
-        JwtModule.register({ secret: process.env.JWT_SECRET, signOptions: { expiresIn: configuration().jwt.signOptions.expiresIn } })
-    ],
+    imports: [ConfigModule, HttpModule, JwtAuthModule],
     controllers: [AuthController],
-    providers: [AuthService, JwtStrategy]
+    providers: [AuthService]
 })
 export class AuthModule {}
