58a38f3a81c9bec1025a34609d6ce8e0a004458d
2-chanhee <l01083582454@gmail.com>
2022-02-06 15:58:48 +0900

    false

--- src/marker/dto/get_markers.dto.ts
+++ src/marker/dto/get_markers.dto.ts
@@ -11,7 +11,7 @@ export class GetMarkersParam {
 }
 
 export class GetMarkersHeaders {
-    @ApiProperty()
+    @ApiProperty({ required: false })
     @IsOptional()
     @IsString()
     readonly authorization?: string;
