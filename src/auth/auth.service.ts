8f1acf3756f9baa10e701b5550cde8e8b3396628
2-chanhee <l01083582454@gmail.com>
2022-02-17 01:51:48 +0900

    ew

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -145,11 +145,7 @@ export class AuthService {
 
     async uploadImage(file) {
         console.log(file);
-        AWS.config.update({
-            region: 'ap-northeast-2',
-            accessKeyId: 'AKIAUZRO6TALVTQZU2V7',
-            secretAccessKey: '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'
-        });
+
         const s3 = new AWS.S3();
         // const s3 = new AWS.S3();
         // const key = `user/thumbnail/${userId}_${Date.now()}`;
