6fadb5fad44b2a9ddd119ddd4b4846d7e028eb2e
2-chanhee <l01083582454@gmail.com>
2022-03-01 21:36:44 +0900

    불필요 seed 제거

--- test/user/user.e2e.seed.ts
+++ test/user/user.e2e.seed.ts
@@ -1,4 +1,4 @@
-import { AuthUser, UserLevel } from '../../src/lib/user_decorator';
+import { UserLevel } from '../../src/lib/user_decorator';
 import { User, UserActive } from '../../src/entities/user.entity';
 
 export const seedImageUploadUser = {
@@ -7,26 +7,3 @@ export const seedImageUploadUser = {
     level: UserLevel.User,
     active: UserActive.Pending
 } as User;
-
-export const seedImageUploadMe = {
-    userId: 1,
-    userLevel: UserLevel.User
-} as AuthUser;
-
-export const file = {
-    fieldname: 'file',
-    originalname: 'test.PNG',
-    encoding: '7bit',
-    mimetype: 'image/png',
-    ACL: 'public-read',
-    ETag: '"1234"',
-    Location: 'test-location',
-    key: '123',
-    Key: '456',
-    Bucket: 'test-bucket',
-    width: 538,
-    height: 384,
-    premultiplied: false,
-    size: 330765,
-    ContentType: 'png'
-};
