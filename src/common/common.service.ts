007ccf6dc4a8202aa43fc81688c2afc7987089d8
2-chanhee <l01083582454@gmail.com>
2022-03-06 16:38:55 +0900

    delete serverless package

--- src/common/common.service.ts
+++ src/common/common.service.ts
@@ -3,7 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class CommonService {
     async getServerStatus() {
-        console.log('process', process.env.stage);
         return 'healthy';
     }
 }
