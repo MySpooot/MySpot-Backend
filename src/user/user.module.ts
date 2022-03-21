9e867067c0adb95841d3c277dadb58ad73e92949
2-chanhee <l01083582454@gmail.com>
2022-03-21 22:14:52 +0900

    adsf

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AwsModule } from 'nest-aws';
 
 import { User } from '../entities/user.entity';
 import { JwtAuthModule } from '../lib/jwt';
@@ -8,7 +9,20 @@ import { UserController } from './user.controller';
 import { UserService } from './user.service';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([User]), JwtAuthModule, ConfigModule],
+    imports: [
+        TypeOrmModule.forFeature([User]),
+        JwtAuthModule,
+        ConfigModule,
+        AwsModule.forRootAsync({
+            imports: [ConfigModule],
+            useFactory: (configService: ConfigService) => ({
+                region: 'ap-northeast-2',
+                accessKeyId: configService.get('aws.access_key_id'),
+                secretAccessKey: configService.get('aws.secret_access_key')
+            }),
+            inject: [ConfigService]
+        })
+    ],
     controllers: [UserController],
     providers: [UserService]
 })
