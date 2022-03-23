aa91e7c8a47c1b2626e73c9e3a70eaa536bde3f3
2-chanhee <l01083582454@gmail.com>
2022-03-24 01:20:50 +0900

    ㅁㄴㅇㄹ

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -16,7 +16,7 @@ export class AuthController {
     @ApiHeader({ name: 'Authorization', required: false })
     @ApiOkResponse({ type: PostLoginResponse })
     login(@Headers() headers: PostLoginHeaders, @Body() body: PostLoginBody) {
-        console.log('tests');
+        console.log('tests333');
         return this.authService.login(headers, body);
     }
 
