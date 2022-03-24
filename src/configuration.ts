f4d9d3552317f979436b57b525ca7168f0a1cba3
2-chanhee <l01083582454@gmail.com>
2022-03-24 22:29:06 +0900

    2

--- src/configuration.ts
+++ src/configuration.ts
@@ -22,28 +22,22 @@ export default () => ({
         region: process.env.REGION
     },
     stage: process.env.stage || 'dev',
-    typeorm: process.env.stage
-        ? {
-              type: 'postgres',
-              autoLoadEntities: true,
-              synchronize: false,
-              bigNumberStrings: false
-          }
-        : process.env.NODE_ENV === 'dev'
-        ? {
-              type: 'postgres',
-              autoLoadEntities: true,
-              synchronize: false,
-              bigNumberStrings: false,
-              ssl: {
-                  rejectUnauthorized: false
+    typeorm:
+        process.env.NODE_ENV === 'test'
+            ? {
+                  type: 'sqlite',
+                  database: ':memory:',
+                  autoLoadEntities: true,
+                  logging: process.env.NODE_ENV === 'test',
+                  synchronize: true
               }
-          }
-        : {
-              type: 'sqlite',
-              database: ':memory:',
-              autoLoadEntities: true,
-              logging: process.env.NODE_ENV === 'local',
-              synchronize: true
-          }
+            : {
+                  type: 'postgres',
+                  autoLoadEntities: true,
+                  synchronize: false,
+                  bigNumberStrings: false,
+                  ssl: {
+                      rejectUnauthorized: false
+                  }
+              }
 });
