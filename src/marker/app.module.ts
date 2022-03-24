c9ddd1971f5efc0dff172276d9d450325514d011
2-chanhee <l01083582454@gmail.com>
2022-03-25 00:15:52 +0900

    asdf

--- src/marker/app.module.ts
+++ src/marker/app.module.ts
@@ -13,7 +13,7 @@ import { MarkerModule } from './marker.module';
             envFilePath: '.env',
             validationSchema: Joi.object({
                 POSTGRES_HOST: Joi.string().required(),
-                POSTGRES_PORT: Joi.string().required(),
+                POSTGRES_PORT: Joi.number().required(),
                 POSTGRES_USERNAME: Joi.string().required(),
                 POSTGRES_PASSWORD: Joi.string().required(),
                 POSTGRES_DATABASE: Joi.string().required()
