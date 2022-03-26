1acabe202951849e5ca1c2cf3f940cb3608bd49b
2-chanhee <54311184+2-chanhee@users.noreply.github.com>
2022-03-26 12:58:29 +0900

    dev/prod lambda, ci/cd env setting 분기 테스트 - dev (#94)

--- src/configuration.ts
+++ src/configuration.ts
@@ -1,21 +1,16 @@
 export default () => ({
-    // database: {
-    //     username: process.env.POSTGRES_USERNAME || '',
-    //     password: process.env.POSTGRES_PASSWORD || '',
-    //     database: process.env.POSTGRES_DATABASE || '',
-    //     host: process.env.POSTGRES_HOST || '',
-    //     port: process.env.POSTGRES_PORT || ''
-    // },
     kakao: {
         clientId: '025b493068d0d400f8c6b9f91b175936',
         localRedirectUrl: 'http://localhost:3000',
         devRedirectUrl: 'https://myspot.netlify.app'
+        // @TODO prod redirect url
     },
     jwt: {
         signOptions: {
             expiresIn: '30d'
         }
     },
+    // for local
     aws: {
         access_key_id: process.env.AWS_ACCESS_KEY_ID,
         secret_access_key: process.env.AWS_SECRET_ACCESS_KEY,
