8860dc71f588fb10568a521dc6641c3c5e817bf7
2-chanhee <l01083582454@gmail.com>
2022-03-27 17:27:21 +0900

    integrated_lambda_server

---
+++ src/lib/server.ts
@@ -0,0 +1,43 @@
+import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import ServerlessExpress from '@vendia/serverless-express';
+import express from 'express';
+
+let cachedServer;
+
+export const bootstrapServer = async (module: any) => {
+    if (!cachedServer) {
+        const expressApp = express();
+
+        const app = await NestFactory.create(module, new ExpressAdapter(expressApp));
+        app.useGlobalPipes(
+            new ValidationPipe({
+                whitelist: true,
+                transform: true,
+                transformOptions: { enableImplicitConversion: true },
+                // disableErrorMessages: If set to true, validation errors will not be returned to the client.
+                disableErrorMessages: false
+            })
+        );
+        // @TODO prod origin setting
+        app.enableCors({
+            origin: '*',
+            allowedHeaders: '*'
+        });
+
+        await app.init();
+
+        // allowed api gateway content-type
+        cachedServer = ServerlessExpress({
+            app: expressApp,
+            binarySettings: {
+                contentTypes: ['image/jpeg', 'image/png', 'image/jpg', 'multipart/form-data']
+            }
+        });
+
+        cachedServer = ServerlessExpress({ app: expressApp });
+    }
+
+    return cachedServer;
+};
