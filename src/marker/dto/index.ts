4785686938303c7cd65fcfdf5e808228df4e5328
2-chanhee <l01083582454@gmail.com>
2022-04-06 22:23:16 +0900

    export types v1.0.0-beta1

---
+++ src/marker/dto/index.ts
@@ -0,0 +1,8 @@
+export type { DeleteMarkerLikeParam } from './delete_marker_like.dto';
+export type { DeleteMarkerParam } from './delete_marker.dto';
+export type { DeleteMyLocationParam } from './delete_my_location.dto';
+export type { GetMarkersParam, GetMarkersHeaders, GetMarkersResponse } from './get_markers.dto';
+export type { GetMyLocationsQuery, GetMyLocationsResponse } from './get_my_locations.dto';
+export type { PostMarkerLikeParam } from './post_marker_like.dto';
+export type { PostMarkerParam, PostMarkerBody, PostMarkerResponse } from './post_marker.dto';
+export type { PostMyLocationBody } from './post_my_location.dto';
---
+++ src/user/dto/index.ts
@@ -0,0 +1,2 @@
+export type { PostUploadImageBody } from './post_upload_image.dto';
+export type { PutUserNicknameBody, PutUserNicknameResponse } from './put_user_nickname.dto';
