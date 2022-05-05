fc22aa3fd245a4d024c8dd9199fee0efd059cafb
2-chanhee <l01083582454@gmail.com>
2022-05-05 12:12:56 +0900

    3

--- index.ts
+++ index.ts
@@ -1,9 +1,14 @@
+export * from './src/auth/auth.module';
 export * from './src/auth/dto';
 
+export * from './src/map/map.module';
 export * from './src/map/dto';
 
+export * from './src/marker/marker.module';
 export * from './src/marker/dto';
 
+export * from './src/reply/reply.module';
 export * from './src/reply/dto';
 
+export * from './src/user/user.module';
 export * from './src/user/dto';
